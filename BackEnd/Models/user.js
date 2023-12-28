65f303d7c5565f9f803131f444bb24903e2bdee1
Em Corkindale <emily.corkindale@gmail.com>
2023-12-28 15:15:43 +1300

    User model finished and test. Password hashing added using node crypto module. Testing completed

--- BackEnd/Models/user.js
+++ BackEnd/Models/user.js
@@ -1,29 +1,57 @@
 const { DataTypes, Model } = require("sequelize");
+const crypto = require("crypto");
 let dbConnect = require("../dbConnect");
 const sequelizeInstance = dbConnect.Sequelize;
-class User extends Model { }
-//Sequelize will create this table if it doesn't exist on startup
-User.init({
-userID: {
-type: DataTypes.INTEGER, allowNull: false, autoIncrement:
-true, primaryKey: true
-},
-firstName: {
-type: DataTypes.STRING, allowNull: false, required: true
-},
-lastName: {
-type: DataTypes.STRING, allowNull: false, required: true
-},
-emailAddress: {
-type: DataTypes.STRING, allowNull: false, required: true,
-unique: true
-},
-password: {
-type: DataTypes.STRING, allowNull: false, required: true
-}},
-{
-sequelize: sequelizeInstance, modelName: 'users', //use lowercase plural format
-timestamps: false, freezeTableName: true
-}
-)
+
+// Function to hash the password using Node.js crypto module (this is irreversable and doesn't store the acual password. User enters their password on login and it will hash it and compare to hashed password)
+const hash = (password) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(password);
+  return hash.digest("hex");
+};
+
+class User extends Model {}
+
+User.init(
+  {
+    userID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      set(value) {
+        // Hashes the password using the defined hash function
+        this.setDataValue("password", hash(value));
+      },
+    },
+  },
+  {
+    sequelize: sequelizeInstance,
+    modelName: "users", // use lowercase plural format
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
 module.exports = User;
