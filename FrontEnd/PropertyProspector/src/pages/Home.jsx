b6a39cbca125943cab3ebfb0e80ee62bd97d1d5b
Em Corkindale <emily.corkindale@gmail.com>
2024-01-03 22:46:55 +1300

    Home page begun with limited bootstrap

---
+++ FrontEnd/PropertyProspector/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function Home() {
+
+    return (
+
+        <Container className='homeComponent'>
+            <Row>
+                <Col>
+                    <h1>Welcome to Property Prospector,<br /> your property matching partner.</h1>
+                </Col>
+                <Col xs lg="2">
+                    <img src={"../src/assets/homeImage.jpg"}></img>
+                </Col>
+            </Row>
+            <Row>
+                <p className="homeText">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> Aperiam fuga tempore nostrum repellat consectetur et facilis minus corrupti qui minima veniam, modi tenetur natus magnam a? Ea sit dolores corporis?</p>
+            </Row>
+        </Container>
+    )
+}
